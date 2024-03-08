a137618df36aa401aa3ebd8a57d0f2ebce5ad26f
Lemi Dinku <lemidinq@gmail.com>
2024-03-08 15:52:39 +0300

    variable and types

---
+++ LAB-1-Variables-and-types-1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Lemi Dinku";
+  int age = 21;
+  String Color = "Black";
+
+  print("My name is $name.");
+  print("I am $age years old.");
+  print("My favorite color is $Color.");
+}
